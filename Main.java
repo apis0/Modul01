ef54eb9d0d3c5a6b84d7ca1b81679c9fa6e2c7bd
hafizakhmad <akhmadhafiz41@gmail.com>
2021-05-25 01:12:43 +0800

    Project IDEA

---
+++ Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main (String [] args){
+        String namaDepan ="Akhmad";
+        String namaTengah ="Hafiz";
+        String namaBelakang ="Ansyari";
+        int usia = 20;
+        int targetTahunKuliah = 4;
+        double ipk = 3.79999999;
+        char nilaiAbjd = 'A';
+        boolean tampan = true;
+        
+        System.out.println(namaDepan + namaTengah + namaBelakang);
+        
+    }
+}
