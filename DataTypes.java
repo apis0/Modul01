c4e393ece579f4ed663a78e9c8387faf0a60ff0e
hafizakhmad <akhmadhafiz41@gmail.com>
2021-05-25 00:33:23 +0800

    deklarasi variabel dan tipe data

---
+++ DataTypes.java
@@ -0,0 +1,14 @@
+public class DataTypes{
+ public static void main (String [] args){
+ 
+  String namaDepan = "Akhmad";
+  String namaTengah = "Hafiz";
+  String NamaBelakang = "Ansyari";
+  int usia = 20;
+  int targetTahunKuliah = 4;
+  double ipk = 3.79999999;
+  char nilaiAbjad = 'A';
+  boolean tampan = true;
+  
+ }
+}
